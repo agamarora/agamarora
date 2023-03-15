7e6eb672d2548553ea951ef597e67cb062524d54
agamarora <agam.arora11@gmail.com>
2023-03-15 18:38:39 +0000

    updated

--- app.py
+++ app.py
@@ -8,39 +8,31 @@ import pandas as pd
 import numpy as np
 
 #title
-st.title("Digital Resume")
+st.title("Agam Arora")
 
 #header
 st.header("About Me")
 
 #about me
-st.write("I am a Data Scientist with a background in Finance and Economics. I am passionate about data and how it can be used to solve real world problems. I am currently working as a Data Scientist at a FinTech company in the Bay Area. I am also a graduate student at the University of California, Berkeley, where I am pursuing a Master's in Information and Data Science. I am also a graduate of the University of California, Los Angeles, where I received my Bachelor's in Economics and a minor in Mathematics.")
+st.write("I am deeply driven by the human ability to reach unlimited skill levels in any pursuit. My work has been shaped by my passion for data, product, and analytics. \n\nIn my current role, I am collecting, unpacking, transforming, and readying data to create measures of success. I enable partner clients to demystify supply chain value levers and help them take data-informed decisions through embedded analytics and data exploration dashboards.\n\nPreviously, I led a team of seven technical product managers to plan, prioritize, and deliver company-wide client ad-hoc requests, ensuring increased NPS and streamlined tech bandwidth. I also raised over $500,000 in government grants for Canadian tech startups in 2020 and increased the retail stores' revenue for a 30- year-old retail company through a mix of digitization, gamification, and analytics in 2019.\n\nMy experience also includes being part of the team behind the 2017 indie game of the year on Google Play (Canada - Beast Brawlers), starting a games development company in India, and developing a text-based web game built over three years using the LAMP tech stack (PHP, MySQL) in 2011.\n\nI resonate highly with the teachings of Geeta and love spending time with my dog. I am always curious and believe that by providing more information, feedback, and using specific language, we can get the most out of any interaction.")
 
 #header
 st.header("Education")
 
 #education
-st.write("University of California, Berkeley, Master of Information and Data Science, 2020 - Present")
-st.write("University of California, Los Angeles, Bachelor of Arts in Economics, 2016 - 2020")
-st.write("University of California, Los Angeles, Minor in Mathematics, 2016 - 2020")
+st.write("FORE School of Management, New Delhi\nPost Graduate Diploma in Management 2012 - 2014\n\nActivities and societies: LAN Gaming, Dancing, Anchoring, Sports\n\nB.M. Institute of Engineering and Technology\nBachelor of Technology (B.Tech.), Computer Science 2008 - 2012\n\nActivities and societies: Theatre, Dance, Music(Vocals), Sports(Basketball, Football, Lawn Tennis, Table Tennis), Quiz Team, Debate Team\n\nHansraj Model School, High School, 1993 - 2008\n\nActivities and societies: Basketball, Football, Swimming, Dancing, Theatre, Debate, Quiz")
 
 #header
 st.header("Work Experience")
 
 #work experience
-st.write("Data Scientist, FinTech Company, 2020 - Present")
-st.write("Data Analyst, FinTech Company, 2019 - 2020")
-st.write("Data Analyst, FinTech Company, 2018 - 2019")
+st.write("Associate Director - Product\nFarEye\nJun 2021 - Present\nNoida, Uttar Pradesh, India\n2 yrs 4 mos\n\nLead product manager - Data engineering and analytics\nFarEye\nRecognition Q4 2021\nWinner Hackathon '21\n\nPlatform Product Manager\nDec 2020 - May 2021\n6 mos\n\nTechnical product manager - Intelligent delivery orchestration\nRecognition Q1 2021\n\nTechnical Product & Program Consultant\nAagaman Consulting · Freelance\nJan 2020 - Dec 2020\n1 yr\n\nManager - Business Development and Expansion\nBlossom Kochhar Beauty Products Pvt. Ltd. · Full-time\nJul 2018 - Dec 2019\n1 yr 6 mos\nNew Delhi Area, India\n\nStudio Head\nV2 Games · Full-time\nJan 2016 - May 2018\n2 yrs 5 mos\nNew Delhi Area, India\n\nData Analyst\nAbsolutdata Analytics · Full-time\nApr 2014 - Dec 2015\n1 yr 9 mos\nGurgaon, India\n\nIntern\nMichael Page · Internship\nApr 2013 - May 2013\n2 mos\nNew Delhi Area, India")
 
 #header
 st.header("Skills")
 
 #skills
-st.write("Python")
-st.write("SQL")
-st.write("Tableau")
-st.write("Excel")
-st.write("R")
+st.write("Many")
 
 #header
 st.header("Projects")
